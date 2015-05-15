33c57d4199fb4d16a23afb6769b4c5143099d9b6
Alexandre JARC <jarc.alex@gmail.com>
2015-05-15 11:51:49 +0200

    Evol du dashboard, ajout modification planification, ajout navigation page

---
+++ modele/modal.php
@@ -0,0 +1,63 @@
+<?php
+function CreateModal ($content, $buttondata, $url) {
+    echo '
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+      </div>
+      <div class="modal-body">
+        <div class="alert alert-info hidden" id="msg"> </div>
+        <form class="ModalForm" id="ModalForm" name="ModalForm" method="post">
+        '.$content.'
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="submit" > Save </button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /.modal -->';
+
+    echo " <script>
+$('#myModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var title = button.data('title')
+  
+  var modal = $(this)
+  modal.find('.modal-title').text(title)
+  
+  ${buttondata}
+
+});
+
+$(document).ready(function () {
+	$('button#submit').click(function(){
+		var modal = $('#myModal')
+		var form = modal.find('.ModalForm')
+		$.ajax({
+			type: 'POST',
+			url: ${url}, // 
+			data: form.serialize(),
+			success: function(msg){
+				$('#msg').html(msg)
+				//$('#msg.hidden').css('visibility','visible').hide().fadeIn().removeClass('hidden');	
+			},
+			error: function(){
+				alert('failure');
+			}
+		});
+		$('#msg.hidden').css('visibility','visible').hide().fadeIn().removeClass('hidden');
+        $('#msg').delay(400).show(500);
+        $('#msg').delay(400).hide(500);
+	});
+});
+</script>";
+
+}
+?>
